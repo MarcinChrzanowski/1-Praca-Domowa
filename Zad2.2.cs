9cd588c43ea1f37bb02515d3d88dfbb076c9e9c5
MarcinChrzanowski <147642407+MarcinChrzanowski@users.noreply.github.com>
2023-10-12 19:36:37 +0200

    Add files via upload

---
+++ Zad2.2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, c, delta;
+        Console.WriteLine("Podaj liczby całkowite:");
+        Console.WriteLine("wspolczynnik a:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("wspolczynnik b:");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("wspolczynnik c:");
+        c = double.Parse(Console.ReadLine());
+        delta = (b * b) - (4 * a * c);
+        Console.WriteLine(delta);
+    }
+}
