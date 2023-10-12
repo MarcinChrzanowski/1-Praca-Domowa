782a958c5c982a96fba53884fb65dc6e5256641e
MarcinChrzanowski <147642407+MarcinChrzanowski@users.noreply.github.com>
2023-10-12 20:03:41 +0200

    Add files via upload

---
+++ Zad2.10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int powierzchnia = 100, osoby = 10;
+        //2 rozne zmienne
+        double gestoscZaludnienia = (double)osoby / (double)powierzchnia;
+        Console.WriteLine(gestoscZaludnienia);
+    }
+}
---
+++ Zad2.9d.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 1, y = 3, z = 4;
+        bool wynik = (x == 1 | y++ > 2 || ++z > 0);
+        Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+    }
+}
