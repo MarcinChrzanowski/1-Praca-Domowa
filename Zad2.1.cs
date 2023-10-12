187a5104702ac711644285dd95828ed40f118266
MarcinChrzanowski <147642407+MarcinChrzanowski@users.noreply.github.com>
2023-10-12 19:23:24 +0200

    Add files via upload

---
+++ Zad2.1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double C, F;
+        Console.WriteLine("Podaj temperature w stopniach Celsjusza: ");
+        C = double.Parse(Console.ReadLine());
+        F = C * 9 / 5+32;
+        Console.WriteLine("Temperatura w stopniach Fahrenheita: "+F);
+    }
+}
