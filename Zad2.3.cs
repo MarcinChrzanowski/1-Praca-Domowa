7205c62b00bb9f4cb7b0b0e70e1cf7b438cbcea2
MarcinChrzanowski <147642407+MarcinChrzanowski@users.noreply.github.com>
2023-10-12 19:42:14 +0200

    Add files via upload

---
+++ Zad2.3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double kg, m, bmi;
+        Console.WriteLine("Podaj mase ciala w kg:");
+        kg = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj wzrost w metrach:");
+        m = double.Parse(Console.ReadLine());
+        bmi = kg/(m*m);
+        Console.WriteLine("BMI wynosi: " + bmi);
+
+    }
+}
